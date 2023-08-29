712aac995d2ae29d0a404f8692f35f75eb076f2c
Thabisile <thabisilemkhize@gmail.com>
2023-08-29 10:45:34 -0700

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Initialize new square.
         Parameter:
-        size: size of new square.
+        size(int): size of new square.
         """
 
         if not isinstance(size, int):
