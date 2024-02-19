7d70a6b5b0c9350750a45eab9cface58ed3673af
thabisile <thabisilemkhize81@gmail.com>
2024-02-19 11:33:38 -0800

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines an inherited list class MyList. """
+
+class MyList(list):
+    """ Implementssorted printing for the built-in list class."""
+    def print_sorted(self):
+    """ Prints a list in sorted ascending order. """
+    print(sorted(self))
