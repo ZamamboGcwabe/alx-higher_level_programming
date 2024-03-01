2ec2d43b36c0ab86ab784421b82cdaa4b36bee2b
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-03-01 19:25:07 +0000

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,7 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import os
 
 
 class TestBase_instantiation(unittest.TestCase):
