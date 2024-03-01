eddfa8a8b8b64dc025fc6abaa2869e8622be274d
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-03-01 19:17:22 +0000

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,82 +1,83 @@
 #!/usr/bin/python3
-"""Define a square class."""
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square."""
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Init a square.
-        Args:
-        id: identity of square.
-        x: co-ordinate of square.
-        y: co-ordinate of square.
-        size: size of square.
-        """
+        """Initialize a new Square.
 
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
         super().__init__(size, size, x, y, id)
-        
+
     @property
     def size(self):
-        """Set size of square."""
+        """Get/set the size of the Square."""
         return self.width
 
-    @sizesetter
+    @size.setter
     def size(self, value):
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Update the square.
+        """Update the Square.
+
         Args:
-        *args: is the list of arguments - no-keyworded arguments
-        1st argument should be the id attribute
-        2nd argument should be the size attribute
-        3rd argument should be the x attribute
-        4th argument should be the y attribute
-        **kwargs: double pointer to a dictionary: key/value (keyworded arguments)
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
         """
-
-        if args in len(args) != 0:
-            i = 0
-            for arg in args
-            if i == 0:
-                if arg is None:
-                    self.__init__(self.size, self.x, self.y)
-                else:
-                    self.id = arg
-            elif i == 1:
-                self.size = arg
-            elif i == 2:
-                self.x = arg
-            elif i == 3:
-                self.y = arg
-            i += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for i, j in kwargs.items():
-                if i == "id":
-                    if j is None:
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
                         self.__init__(self.size, self.x, self.y)
                     else:
-                        self.id = j
-                elif i == "size":
-                    self.size = j
-                elif i == "x":
-                    self.x = j
-                elif i == "y":
-                    self.y = j
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-            """Return the dictionary rep of square."""
-            return {
-                    "id": self.id,
-                    "size": self.width,
-                    "x": self.x,
-                    "y": self.y
-                    }
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
 
-            def __str__(self):
-                """Return print and str rep of square."""
-                return "[Square] ({}) {}/{} - {}".format(self.id, self.width, self,x, self.y)
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
