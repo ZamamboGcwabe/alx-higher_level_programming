350b570f3c14ee2ca9e3269ac0de81a7c0042bc5
Thabisile <thabisilemkhize@gmail.com>
2024-04-22 15:23:12 -0700

    Addition

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+function add(a, b) {
+	const sum = a + b;
+	console.log(sum);
+}
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+	console.log("Missing arguments");
+} else {
+	const firstNum = parseInt(args[0]);
+	const secondNum = parseInt(args[1]);
+
+	if (isNaN(firstNum) || isNaN(secondNum)) {
+		console.log("Please provide valid numbers");
+	} else {
+		add(firstNum, secondNum);
+	}
+}
+
