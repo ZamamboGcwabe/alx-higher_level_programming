47d48901d17fb06914c69216323e8cc05181e5ee
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-22 23:27:52 +0100

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+function factorial(num) {
+  if (isNaN(num)) {
+    return 1;
+  } else if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+
+if (isNaN(num)) {
+  console.log("Please provide a valid number");
+} else {
+  const result = factorial(num);
+  console.log(`The factorial of ${num} is ${result}`);
+}
