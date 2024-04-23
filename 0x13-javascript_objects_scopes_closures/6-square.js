3fa649c18a4641c1b16164800bf27bab8d92d2ac
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-23 23:32:08 +0100

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+  
+  charPrint(char = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
