b3103eb149961616bc9a780b05707a81aa1ac116
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-23 08:05:39 +0100

    4-concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -5,5 +5,5 @@ const args = process.argv.slice(2);
 if (args.length >= 2) {
 	console.log('${args[0]} is ${args[1]}');
 } else {
-	console.log("Not enough arguments");
+	console.log('Not enough arguments');
 }
