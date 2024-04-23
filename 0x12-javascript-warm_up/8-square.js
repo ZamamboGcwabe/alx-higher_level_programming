6621dd79e9e5c9e5be265cf98badb4e16eeb91e6
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-23 08:07:48 +0100

    8-square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const size =parseInt(args[0]);
 
 if (isNaN(size)) {
-	console.log("Missing size");
+	console.log('Missing size');
 } else {
 	for (let i = 0;i < size; i++) {
 		let row = "";
