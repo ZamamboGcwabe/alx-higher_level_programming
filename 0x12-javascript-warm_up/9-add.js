677720b7fe58305e5a699b9c2431e3a13c415119
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-23 08:08:31 +0100

    9-add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,13 +8,13 @@ function add(a, b) {
 const args = process.argv.slice(2);
 
 if (args.length < 2) {
-	console.log("Missing arguments");
+	console.log('Missing arguments');
 } else {
 	const firstNum = parseInt(args[0]);
 	const secondNum = parseInt(args[1]);
 
 	if (isNaN(firstNum) || isNaN(secondNum)) {
-		console.log("Please provide valid numbers");
+		console.log('Please provide valid numbers');
 	} else {
 		add(firstNum, secondNum);
 	}
