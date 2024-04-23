b9e6afe24d5e5b014457ae657b548f01e6e802b4
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-23 23:39:48 +0100

    10-converter.js

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  return function (num) {
+    if (num === 0) return '0';
+    if (base === 1) return num.toString();
+    
+    const remainder = num % base;
+    const quotient = Math.floor(num / base);
+    const remainderChar = remainder < 10 ? remainder.toString() : String.fromCharCode(remainder + 55);
+    return `${exports.converter(base)(quotient)}${remainderChar}`;
+  };
+};
