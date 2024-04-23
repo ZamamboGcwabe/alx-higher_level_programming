5ff4f774178ce795007168a3301aedae2e8e677c
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-23 08:07:21 +0100

    7-multi_c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const args = process.argv.slice(2);
 const numOccurrences = parseInt(args[0]);
 
 if (isNaN(numOccurrences)) {
-	console.log("Missing number of occurrences");
+	console.log('Missing number of occurrences');
 } else {
 	for (let i = 0; i < numOccurrences; i++) {
-		console.log("C is fun");
+		console.log('C is fun');
 	}
 }
