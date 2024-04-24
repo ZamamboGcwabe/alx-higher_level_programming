aa095b48d51090f273daeb527292a0b8f0c0e8da
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-24 17:37:07 +0100

    13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,20 +1,5 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  """
-  This function adds two integers and returns the sum.
-
-  Args:
-      a: The first integer.
-      b: The second integer.
-
-  Returns:
-      The sum of a and b.
-  """
+exports.add = function add(a, b) {
   return a + b;
-}
-
-const num1 = 5;
-const num2 = 3;
-const result = add(num1, num2);
-console.log(`The sum of ${num1} and ${num2} is ${result}`);
+};
