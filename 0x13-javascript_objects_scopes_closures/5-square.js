d53a2f208c18b7c9f63fc54f20c7ae55de45d650
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-04-24 09:45:04 +0100

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
+const Rectangle = require('./4-rectangle');
+
 class Square extends Rectangle {
   constructor(size) {
     super(size, size);
@@ -7,3 +9,4 @@ class Square extends Rectangle {
 }
 
 module.exports = Square;
+
