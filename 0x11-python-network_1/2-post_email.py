ad3549dc47ea1e29fd551927f73c88e958de8ad3
Thabisile <thabisilemkhize@gmail.com>
2024-05-10 14:28:39 -0700

    Post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter,
+and displays the body of the response
+"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__== "__main__":
+    argv =nsys.argv
+    url = argv[1]
+    email = argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DAATA = DATA.encode("ascii")
+    with urllib.request.urlopen(url, DATA) as response:
+        print(response.read().decode("utf-8"))
