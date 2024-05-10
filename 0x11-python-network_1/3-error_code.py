8e272827e2ae089c5e99fd65dfc2a9ced59d6b0b
Thabisile <thabisilemkhize@gmail.com>
2024-05-10 14:49:47 -0700

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and
+displays the body of the response
+"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    argv = sy.argv
+    try:
+        with request.urlopen(url)as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.status))
