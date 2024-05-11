91153cfc587f1f902ef6b17501c2cb203a93218c
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-05-11 13:49:25 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.uropen(request) as response:
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
