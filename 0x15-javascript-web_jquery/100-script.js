7b03a39f9d3e9bc10be7060a94d2b5030b825e10
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-06-14 18:35:12 +0100

    100-script.js

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,5 +1,21 @@
 // Select the <header> element
 // Update the text color to red
 
-const header = document.querySelector('header');
-header.style.color = '#FF0000';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Header Color</title>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const header = document.querySelector('header');
+            
+            header.style.color = '#FF0000';
+        });
+    </script>
+</head>
+<body>
+    <header>This is the header</header>
+</body>
+</html>
